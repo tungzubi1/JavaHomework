f2a26deda09aa2950c3fbbc42faab1a70463dadc
tungzubi1 <tunglt22@onemount.com>
2021-05-18 00:20:44 +0700

    Homework-20210518_v1.1
    Trim dấu cách khi đêm từ trong chuỗi

--- Java_day4/src/bai8.java
+++ Java_day4/src/bai8.java
@@ -5,10 +5,11 @@ public class bai8 {
         int a,b =0, loop = 0;
         int c = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập 1 chuỗi bất kỳ");
+        System.out.print("Nhập 1 chuỗi bất kỳ: ");
         String str = sc.nextLine();
+        String strauth = str.replaceAll("\\s+"," ");
         while (b >= 0 && b <= str.length()){
-            a = str.indexOf(" ", b);
+            a = strauth.indexOf(" ", b);
             if(a==-1){
                 b=a;
             }else {
@@ -16,7 +17,7 @@ public class bai8 {
                 loop += 1;
                 c = loop + 1;
             }
-        } if (loop > 0) System.out.println("Chuỗi '"+str+"' có "+c+" từ");
+        } if (loop > 0) System.out.println("Chuỗi '"+strauth+"' có "+c+" từ");
         else System.out.println("Chuỗi chỉ có 1 từ hoặc không có từ nào");
     }
 }
