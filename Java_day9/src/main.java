8dda60b997a4dd1548ecda8dbf5dcff7681cc152
tungzubi1 <tunglt22@onemount.com>
2021-05-29 00:09:56 +0700

    Làm được rùi

--- Java_day9/src/main.java
+++ Java_day9/src/main.java
@@ -5,17 +5,19 @@ public class main {
         bai2();
     }
 
-
     static void bai2(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhập chuỗi: ");
         String str = sc.nextLine();
         String str2 = new String();
         for(int i=0; i<str.length(); i++){
+            int count=0;
             for(int j=0; j<str2.length(); j++){
-                if (str.charAt(i)!=str2.charAt(j)){
-                    str2 = str2 + str.charAt(i);
+                if (str.charAt(i) == str2.charAt(j)){
+                    count++;
                 }
+            } if(count == 0){
+                str2 = str2 + str.charAt(i);
             }
         }
         System.out.println(str2.length());
